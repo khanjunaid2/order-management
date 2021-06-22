1588fc6efc87c835bfc4617177852b8c12011dbc
prajakta25 <prajaktarajhans96@gmail.com>
2021-06-22 11:12:43 -0500

    feat(module2) : add swagger config

--- src/main/java/com/egen/controller/CustomerController.java
+++ src/main/java/com/egen/controller/CustomerController.java
@@ -3,6 +3,8 @@ package com.egen.controller;
 import com.egen.model.Customer;
 import com.egen.model.Order;
 import com.egen.service.CustomerService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
@@ -11,23 +13,27 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@Api(value = "/customer",description = "Performing operations on Customer ")
 public class CustomerController {
 
     @Autowired
     CustomerService customerService;
 
     @GetMapping("customer")
-    public ResponseEntity<List<Customer>> getAllOrders(){
+    @ApiOperation(value = "Find all customer",notes = "Returns list of all customer")
+    public ResponseEntity<List<Customer>> getAllCustomers(){
         return ResponseEntity.ok(customerService.findAll());
     }
 
     @GetMapping("customer/{id}")
-    public ResponseEntity<Customer> getOrderById(@PathVariable("id") long id){
+    @ApiOperation(value = "Find customer by id",notes = "Returns customer using customer id")
+    public ResponseEntity<Customer> getCustomerById(@PathVariable("id") long id){
         Customer cust = customerService.getById(id);
         return ResponseEntity.ok(cust);
     }
 
     @PostMapping("customer")
+    @ApiOperation(value = "Create customer",notes = "Insert new customer data")
     public ResponseEntity<Customer> create(@RequestBody Customer cust){
         Customer obj = customerService.create(cust);
         return ResponseEntity.ok(obj);
@@ -35,12 +41,14 @@ public class CustomerController {
 
 
     @DeleteMapping("customer/{id}")
+    @ApiOperation(value = "Delete customer",notes = "Remove customer data")
     public ResponseEntity delete(@PathVariable("id") long id){
         customerService.delete(id);
         return (ResponseEntity) ResponseEntity.ok();
     }
 
     @PutMapping("customer/{id}")
+    @ApiOperation(value = "Update customer",notes = "Update customer data")
     public ResponseEntity<Customer> update(@PathVariable("id") long id,  @RequestBody Customer cust){
         return  ResponseEntity.ok(customerService.update(cust));
     }
--- src/main/java/com/egen/controller/OrderController.java
+++ src/main/java/com/egen/controller/OrderController.java
@@ -2,6 +2,8 @@ package com.egen.controller;
 
 import com.egen.model.Order;
 import com.egen.service.OrderService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,17 +14,20 @@ import java.util.Collections;
 import java.util.List;
 
 @RestController
+@Api(value = "/order",description = "Performing operations on Order ")
 public class OrderController {
 
     @Autowired
     OrderService orderService;
 
     @GetMapping("order")
+    @ApiOperation(value = "Find all orders",notes = "Returns list of all orders")
     public ResponseEntity<List<Order>> getAllOrders(){
         return ResponseEntity.ok(orderService.getAllOrders());
     }
 
     @GetMapping("order/{id}")
+    @ApiOperation(value = "Find order by id",notes = "Returns order using order id")
     public ResponseEntity<Order> getOrderById(@PathVariable("id") long id){
         Order order = orderService.getOrderById(id);
         return ResponseEntity.ok(order);
@@ -40,6 +45,7 @@ public class OrderController {
     }
 
     @PostMapping("order")
+    @ApiOperation(value = "Create order",notes = "Place new order")
     public ResponseEntity<Order> placeOrder(@RequestBody  Order order){
         Order obj = orderService.placeOrder(order);
         return ResponseEntity.ok(obj);
@@ -47,11 +53,13 @@ public class OrderController {
 
 
     @PutMapping("order/cancel/{id}")
+    @ApiOperation(value = "Cancel order",notes = "Cancel order by changing the status")
     public ResponseEntity<Order> cancelOrder(@PathVariable("id") long id){
         return ResponseEntity.ok(orderService.cancelOrder(id));
     }
 
     @PutMapping("order/{id}")
+    @ApiOperation(value = "Update order",notes = "Update order data")
     public ResponseEntity<Order> updateOrder(@PathVariable("id") long id,  @RequestBody Order order){
         return  ResponseEntity.ok(orderService.updateOrder(order));
     }
